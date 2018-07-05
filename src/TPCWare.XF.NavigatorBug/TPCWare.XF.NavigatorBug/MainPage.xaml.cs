1e53cb0e904fa61d9366d47490521e78dd52e135
Nicolò Carandini <n.carandini@outlook.com>
2018-07-05 19:56:46 +0200

    Create demo app

---
+++ src/TPCWare.XF.NavigatorBug/TPCWare.XF.NavigatorBug/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TPCWare.XF.NavigatorBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoNextPageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ src/TPCWare.XF.NavigatorBug/TPCWare.XF.NavigatorBug/ThirdPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TPCWare.XF.NavigatorBug
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ThirdPage : ContentPage
+	{
+		public ThirdPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void GoBackButton_Clicked(object sender, EventArgs e)
+        {
+            var secondPage = Navigation.NavigationStack.LastOrDefault(p => p is SecondPage);
+
+            if (secondPage != null)
+            {
+                Navigation.RemovePage(secondPage);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+}
